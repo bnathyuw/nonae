29e9e364a7077faba5651795b7871aa7186637b0
matthew butt <matthew.butt@7digital.com>
2013-05-23 11:52:29 +0100

    A bit of renaming

--- Nonae.Core/HttpHandler.cs
+++ Nonae.Core/HttpHandler.cs
@@ -6,13 +6,13 @@ namespace Nonae.Core
 {
 	public class HttpHandler : IHttpHandler
 	{
-		private readonly Dictionary<string, Endpoint> _allow = new Dictionary<string, Endpoint>();
+		private readonly Dictionary<string, Endpoint> _endpoints = new Dictionary<string, Endpoint>();
 
 		public void ProcessRequest(HttpContext context)
 		{
 			var path = context.Request.Path;
 
-			var endpoint = GetBlah(path);
+			var endpoint = FindEndpoint(path);
 
 			if (endpoint == null)
 			{
@@ -33,9 +33,9 @@ namespace Nonae.Core
 			context.Response.Headers.Add("Allow", allowHeader);
 		}
 
-		private Endpoint GetBlah(string path)
+		private Endpoint FindEndpoint(string path)
 		{
-			return _allow.ContainsKey(path) ? _allow[path] : null;
+			return _endpoints.ContainsKey(path) ? _endpoints[path] : null;
 		}
 
 		public bool IsReusable { get { return true; } }
@@ -43,7 +43,7 @@ namespace Nonae.Core
 		protected Endpoint AddEndpoint(string url)
 		{
 			var endpoint = new Endpoint();
-			_allow.Add(url, endpoint);
+			_endpoints.Add(url, endpoint);
 			return endpoint;
 		}
 	}
