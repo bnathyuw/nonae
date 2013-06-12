c5034dd4ce1d2e3b2b600e8318b50233a7e0fbf2
matthew butt <matthew.butt@7digital.com>
2013-06-12 17:54:47 +0100

    Tests for AuthorizationHandler

---
+++ Nonae.Tests.Unit/Handlers/AuthorizationHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Nonae.Core.Handlers;
+using Nonae.Core.Results;
+using Rhino.Mocks;
+
+namespace Nonae.Tests.Unit.Handlers
+{
+	[TestFixture]
+	public class AuthorizationHandlerTests
+	{
+		private IHandler _successor;
+		private AuthorizationHandler _handler;
+		private IRequestDetails _requestDetails;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_successor = MockRepository.GenerateStub<IHandler>();
+			_handler = new AuthorizationHandler(_successor);
+			_requestDetails = MockRepository.GenerateStub<IRequestDetails>();
+		}
+
+		[Test]
+		public void Returns_result_from_successor_if_the_request_is_authorized()
+		{
+			_requestDetails.Stub(rd => rd.IsAuthorized).Return(true);
+			var expectedResult = MockRepository.GenerateStub<IResult>();
+			_successor.Stub(s => s.Handle(_requestDetails)).Return(expectedResult);
+
+			var result = _handler.Handle(_requestDetails);
+
+			_successor.AssertWasCalled(s => s.Handle(_requestDetails));
+			Assert.That(result, Is.EqualTo(expectedResult));
+		}
+
+		[Test]
+		public void Returns_unauthorized_if_the_request_is_not_authorized()
+		{
+			_requestDetails.Stub(rd => rd.IsAuthorized).Return(false);
+
+			var result = _handler.Handle(_requestDetails);
+
+			Assert.That(result, Is.TypeOf<UnauthorizedResult>());
+		}
+	}
+}
