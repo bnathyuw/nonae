8c7d9c7a59f33ce5c74ed82621bac892cae988ef
matthew butt <matthew.butt@7digital.com>
2013-06-13 15:03:53 +0100

    Options Handler tests

---
+++ Nonae.Tests.Unit/Handlers/OptionsHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Nonae.Core.Handlers;
+using Nonae.Core.Results;
+using Rhino.Mocks;
+
+namespace Nonae.Tests.Unit.Handlers
+{
+	[TestFixture]
+	public class OptionsHandlerTests
+	{
+		private IHandler _successor;
+		private OptionsHandler _handler;
+		private IRequestDetails _requestDetails;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_successor = MockRepository.GenerateStub<IHandler>();
+			_handler = new OptionsHandler(_successor);
+			_requestDetails = MockRepository.GenerateStub<IRequestDetails>();
+		}
+
+		[Test]
+		public void Returns_options_result_if_the_request_method_is_options()
+		{
+			_requestDetails.Stub(rd => rd.IsOptionsRequest).Return(true);
+
+			var result = _handler.Handle(_requestDetails);
+
+			Assert.That(result, Is.TypeOf<OptionsResult>());
+		}
+
+		[Test]
+		public void Returns_result_from_successor_if_the_request_method_is_not_options()
+		{
+			_requestDetails.Stub(rd => rd.IsOptionsRequest).Return(false);
+			var expectedResult = MockRepository.GenerateStub<IResult>();
+			_successor.Stub(s => s.Handle(_requestDetails)).Return(expectedResult);
+
+			var result = _handler.Handle(_requestDetails);
+
+			_successor.AssertWasCalled(s => s.Handle(_requestDetails));
+			Assert.That(result, Is.EqualTo(expectedResult));
+		}
+	}
+}
