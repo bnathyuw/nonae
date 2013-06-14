7268db65ad6d3ecbc4aab3df6f166b4646518ace
matthew butt <matthew.butt@7digital.com>
2013-06-14 18:11:44 +0100

    More endpoint tests

--- Nonae.Tests.Unit/Endpoints/EndpointTests.cs
+++ Nonae.Tests.Unit/Endpoints/EndpointTests.cs
@@ -8,6 +8,8 @@ namespace Nonae.Tests.Unit.Endpoints
 	public class EndpointTests
 	{
 		private Endpoint _nullEndpoint;
+		private Endpoint _endpoint;
+		private const string Url = "Url";
 
 		[Test]
 		public void Null_endpoint_does_not_exist()
@@ -15,10 +17,11 @@ namespace Nonae.Tests.Unit.Endpoints
 			Assert.That(_nullEndpoint.Exists, Is.False);
 		}
 
-		[TestFixtureSetUp]
-		public void TestFixtureSetUp()
+		[SetUp]
+		public void SetUp()
 		{
 			_nullEndpoint = Endpoint.Null();
+			_endpoint = Endpoint.AtUrl(Url);
 		}
 
 		[Test]
@@ -41,6 +44,58 @@ namespace Nonae.Tests.Unit.Endpoints
 			Assert.That(_nullEndpoint.Allows(HttpMethod.Head), Is.False);
 			Assert.That(_nullEndpoint.Allows(HttpMethod.Post), Is.False);
 			Assert.That(_nullEndpoint.Allows(HttpMethod.Put), Is.False);
+		}
+
+		[Test]
+		public void Endpoint_is_authorized_for_any_credentials()
+		{
+			Assert.That(_endpoint.IsAuthorizedFor(null), Is.True);
+		}
+
+		[Test]
+		public void Endpoint_is_at_configured_url()
+		{
+			Assert.That(_endpoint.IsAt(Url), Is.True);
+		}
+
+		[Test]
+		public void Endpoint_with_no_methods_configured_has_empty_allow_header()
+		{
+			Assert.That(_endpoint.AllowHeader, Is.EqualTo(""));
+		}
+
+		[Test]
+		public void Endpoint_with_no_methods_configured_does_not_allow_any_methods()
+		{
+			Assert.That(_endpoint.Allows(HttpMethod.Delete), Is.False);
+			Assert.That(_endpoint.Allows(HttpMethod.Get), Is.False);
+			Assert.That(_endpoint.Allows(HttpMethod.Head), Is.False);
+			Assert.That(_endpoint.Allows(HttpMethod.Post), Is.False);
+			Assert.That(_endpoint.Allows(HttpMethod.Put), Is.False);
+		}
+
+		[Test]
+		public void Endpoint_with_configured_methods_has_correct_allow_header()
+		{
+			var endpoint = _endpoint.WithMethods(HttpMethod.Get, HttpMethod.Delete);
+
+			Assert.That(endpoint.AllowHeader, Is.StringContaining("DELETE"));
+			Assert.That(endpoint.AllowHeader, Is.StringContaining("GET"));
+			Assert.That(endpoint.AllowHeader, Is.Not.StringContaining("HEAD"));
+			Assert.That(endpoint.AllowHeader, Is.Not.StringContaining("POST"));
+			Assert.That(endpoint.AllowHeader, Is.Not.StringContaining("PUT"));
+		}
+
+		[Test]
+		public void Endpoint_with_configured_methods_allows_correct_methods()
+		{
+			var endpoint = _endpoint.WithMethods(HttpMethod.Get, HttpMethod.Delete); 
+			
+			Assert.That(endpoint.Allows(HttpMethod.Delete), Is.True);
+			Assert.That(endpoint.Allows(HttpMethod.Get), Is.True);
+			Assert.That(endpoint.Allows(HttpMethod.Head), Is.False);
+			Assert.That(endpoint.Allows(HttpMethod.Post), Is.False);
+			Assert.That(endpoint.Allows(HttpMethod.Put), Is.False);
 		}
 	}
 }
