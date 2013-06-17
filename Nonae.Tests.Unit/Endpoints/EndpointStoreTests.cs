d77092edb82aee8b525197a4c6d77a5c9e5cb0ca
matthew butt <matthew.butt@7digital.com>
2013-06-17 11:18:27 +0100

    Unit test EndpointStore

---
+++ Nonae.Tests.Unit/Endpoints/EndpointStoreTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Nonae.Core.Endpoints;
+
+namespace Nonae.Tests.Unit.Endpoints
+{
+	[TestFixture]
+	public class EndpointStoreTests
+	{
+		private EndpointStore _endpointStore;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_endpointStore = new EndpointStore();
+		}
+
+		[Test]
+		public void Returns_endpoint_when_it_exists()
+		{
+			var createdEndpoint = Endpoint.AtUrl("/test/url");
+			_endpointStore.Add(createdEndpoint);
+
+			var foundEndpoint = _endpointStore.Get("/test/url");
+
+			Assert.That(foundEndpoint, Is.EqualTo(createdEndpoint));
+		}
+
+		[Test]
+		public void Returns_null_endpoint_when_it_does_not_exist()
+		{
+			var foundEndpoint = _endpointStore.Get("/test/url");
+
+			Assert.That(foundEndpoint.Exists, Is.False);
+		}
+	}
+}
