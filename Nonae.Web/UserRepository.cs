59a6f5ad421d74ad4bfebe2f6ce7ebfd338892c3
matthew butt <matthew.butt@7digital.com>
2013-07-11 11:03:48 +0100

    User an in-memory store in the UserRepository

--- Nonae.Web/UserRepository.cs
+++ Nonae.Web/UserRepository.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Nonae.Core.Handlers;
 
 namespace Nonae.Web
 {
 	public class UserRepository : IResourceRepository
 	{
+		private static readonly IEnumerable<int> Users;
+
+		static UserRepository()
+		{
+			Users = new List<int> {1,2,3,4};
+		}
+
 		public bool Exists(Dictionary<string, string> query)
 		{
-			return query["id"] != "666";
+			var userId = int.Parse(query["id"]);
+			return Users.Any(u => u == userId);
 		}
 	}
 }
